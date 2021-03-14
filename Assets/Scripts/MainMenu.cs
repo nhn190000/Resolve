72770551ccce4b515f019f2d0a850b3a5d8a8d0b
nhn190000 <77559898+nhn190000@users.noreply.github.com>
2021-03-14 00:57:12 -0600

    implement menu and options

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,9 +4,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Image options;
+    public Image menu;
     public void StartGameButton()
     {
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void OptionsButton()
+    {
+        menu.gameObject.SetActive(false);
+        options.gameObject.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        options.gameObject.SetActive(false);
+        menu.gameObject.SetActive(true);
     }
 
     public void QuitGameButton()
@@ -14,4 +28,3 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 }
-    
